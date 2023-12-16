fd14cd2ff8b3334ddc8bcff20c822585b4fbb32d
SkIttER <sharvad_skitter@mail.ru>
2023-12-16 15:03:22 +0300

    feat: added cart

--- lab/scripts/menuEdit.js
+++ lab/scripts/menuEdit.js
@@ -3,7 +3,6 @@
         const loc = window.location.pathname.split('/').at(-1);
 
         let menuItems = document.querySelectorAll('.navigation-element > a');
-        console.log(menuItems)
 
         menuItems.forEach(function (item) {
             if (item.getAttribute('href') === loc) {
