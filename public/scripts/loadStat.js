316a5bdcb10aef7a88e4a7ef26d0a696e6add7c5
SkIttER <sharvad_skitter@mail.ru>
2024-03-08 21:41:33 +0300

    feat: added server time

--- public/scripts/loadStat.js
+++ public/scripts/loadStat.js
@@ -3,10 +3,9 @@ const startTime = Date.now();
 window.addEventListener("load", (() => {
     const loadTime = Date.now() - startTime
     let element = document.querySelector('footer div');
-    element.textContent = `Время загрузки: ${loadTime} мс`;
+    element.textContent = `Время загрузки: ${loadTime} мс + ${serverProcessingTime} мс`;
     setTimeout(() => {
         element.style.color = '#1c1c2f';
         element.style.transition = 'color 1s ease'
     }, 5000)
-
 }))
